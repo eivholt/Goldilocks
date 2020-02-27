e86e6e6dc3fa8dd9564b0a8afcef94e3761e403d
eih <eih@dips.no>
2020-02-27 10:56:54 +0100

    Google Healthcare resources

---
+++ Google Healthcare/ExecuteBundle.ps1
@@ -0,0 +1,13 @@
+﻿$cred = gcloud auth print-access-token
+$headers = @{ Authorization = "Bearer $cred" }
+$PROJECT_ID = "iothealthcare-269209"
+$LOCATION = "europe-west4"
+$DATASET_ID = "IoTHealthcareDataset"
+$FHIR_STORE_ID = "ResourcesDataStore"
+
+Invoke-RestMethod `
+  -Method Post `
+  -Headers $headers `
+  -ContentType: "application/fhir+json" `
+  -InFile $PSScriptRoot"\Example resources\fhir.stu3.resource.beds.example.bundle.json" `
+  -Uri "https://healthcare.googleapis.com/v1beta1/projects/$PROJECT_ID/locations/$LOCATION/datasets/$DATASET_ID/fhirStores/$FHIR_STORE_ID/fhir" | ConvertTo-Json
